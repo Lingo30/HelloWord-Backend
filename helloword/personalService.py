f58b45573bdf791750236c256ab54207f2e4d95a
Mmmusel <20373363@buaa.edu.cn>
2023-05-21 17:37:00 +0800

    feat: 主题词生成词单api

--- helloword/personalService.py
+++ helloword/personalService.py
@@ -13,6 +13,50 @@ import pdfplumber
 import re
 from zhon.hanzi import punctuation
 
+# TODO:增加id及次数控制、加锁
+def get_wordlist_from_tags(request):
+    response = {}
+    response['state'] = False
+
+    try:
+        data = json.loads(request.body.decode())
+        user_id = data.get('userId')
+        tags = data.get('tags')
+
+        message = wordlist.gen_wordlist_from_keywords(tags)
+        rcv_message = client.Client().send_message(message)
+        print(rcv_message)
+        extract_words = str(re.findall(r"[\[](.*?)[\]]", rcv_message)[0]).split(', ')
+
+        lower_extract_words=[]
+        for i in extract_words:
+            lower_extract_words.append(i.lower())
+
+        response['words']=list(set(lower_extract_words))
+
+        ret = []
+        word_list = Word.objects.filter(word__in=lower_extract_words)
+        if word_list.count()==0:
+            response['msg']='所给关键词未生成有效单词，换一个主题词吧~'
+            return JsonResponse(response)
+
+        for k in word_list:
+            cur = {
+                'wordId':k.id,
+                'word':k.word,
+                'meaning':k.definition_cn.replace("\\n","\n").replace("\\r","")
+            }
+            ret.append(cur)
+
+        response['wordlist']=ret
+        response['state'] = True
+        response['msg'] = '词单生成成功'
+
+
+    except Exception as e:
+        response['msg'] = str(e)
+    return JsonResponse(response)
+
 
 def get_wordList_smart_from_file(request):
 
@@ -69,7 +113,7 @@ def get_wordList_smart_from_file(request):
 
         response['wordlist']=ret
         response['state'] = True
-        response['msg'] = '词单导入成功'
+        response['msg'] = '词单提取成功'
 
 
     except Exception as e:
