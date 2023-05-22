6be48d0803db7a0817bf62e0139f84a462e1854d
zhuole1025 <zhuole1025@gmail.com>
2023-05-23 00:17:20 +0800

    fix: 增加输入文本长度限制

--- chatgpt/tools/utils.py
+++ chatgpt/tools/utils.py
@@ -62,3 +62,38 @@ def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301"):
                 num_tokens += tokens_per_name
     num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
     return num_tokens
+
+def truncate_text(text, max_tokens, model="gpt-3.5-turbo-0301"):
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        print("Warning: model not found. Using cl100k_base encoding.")
+        encoding = tiktoken.get_encoding("cl100k_base")
+    if model == "gpt-3.5-turbo":
+        print("Warning: gpt-3.5-turbo may change over time. Returning num tokens assuming gpt-3.5-turbo-0301.")
+        return truncate_text(text, max_tokens, model="gpt-3.5-turbo-0301")
+    elif model == "gpt-4":
+        print("Warning: gpt-4 may change over time. Returning num tokens assuming gpt-4-0314.")
+        return truncate_text(text, max_tokens, model="gpt-4-0314")
+    elif model == "gpt-3.5-turbo-0301":
+        tokens_per_message = 4  # every message follows {role/name}\n{content}\n
+        tokens_per_name = -1  # if there's a name, the role is omitted
+    elif model == "gpt-4-0314":
+        tokens_per_message = 3
+        tokens_per_name = 1
+    else:
+        raise NotImplementedError(f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens.""")
+    
+    # 分词文本
+    tokens = encoding.encode(text)
+    token_count = len(tokens)
+
+    if token_count <= max_tokens:
+        # 文本的 token 数在限制范围内，无需截断
+        return text
+
+    # 截断文本
+    truncated_tokens = tokens[:max_tokens]
+    truncated_text = encoding.decode(truncated_tokens)
+
+    return truncated_text
