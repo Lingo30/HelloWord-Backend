022b33850b65df49254f76699f2cb96b5d5fd142
zhuole1025 <zhuole1025@gmail.com>
2023-05-24 00:24:04 +0800

    fix: 对接语音

--- chatgpt/tools/voice.py
+++ chatgpt/tools/voice.py
@@ -5,8 +5,5 @@ class Voice:
         self.synthetic_voice = TTS(model_name = TTS.list_models()[0], progress_bar=False, gpu=False) 
         
     def speak(self, text, file_name='output.wav'):
-        import os
-        print(os.getcwd())
-        print('../backend_static/user_voice/' + file_name)
         self.synthetic_voice.tts_to_file(text=text, speaker=self.synthetic_voice.speakers[0], language=self.synthetic_voice.languages[0], file_path='../backend_static/user_voice/' + file_name, speed=1.0)
         return 
