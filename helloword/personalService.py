0f73ea6183cbbf169cb9080b3597ea0000244891
Musel <mmmusel@163.com>
2023-06-03 13:01:56 +0800

    feat: 词单gpt服务加锁

--- helloword/personalService.py
+++ helloword/personalService.py
@@ -19,11 +19,28 @@ dailly_times = 5
 def get_wordlist_from_tags(request):
     response = {}
     response['state'] = False
+    data = json.loads(request.body.decode())
+    user_id = data.get('userId')
 
     try:
-        data = json.loads(request.body.decode())
-        user_id = data.get('userId')
         tags = data.get('tags')
+
+        if not checkCookie(request,response,user_id):
+            return JsonResponse(response)
+        user_obj = UserInfo.objects.get(id=user_id)
+
+        times_left = dailly_times - FileInfo.objects.filter(user_id_id=user_obj,
+                                                            post_time__gte=datetime.date.today()).count()
+
+        if user_obj.vip_time and user_obj.vip_time > datetime.datetime.now():
+            times_left += 2
+
+        if times_left == 0:
+            response['last_times'] = 0
+            response['msg'] = '今天的词单创建次数已经用完啦！明天再来吧'
+            user_obj.gpt_lock = ""
+            user_obj.save()
+            return JsonResponse(response)
 
         message = wordlist.gen_wordlist_from_keywords(tags)
         rcv_message = client.Client().send_message(message)
@@ -52,9 +69,28 @@ def get_wordlist_from_tags(request):
 
         response['wordlist']=ret
         response['state'] = True
-        response['msg'] = '词单生成成功'
+
+        file = open("../file_tmp.txt",'w')
+
+        newfile = FileInfo(file_info=file, user_id=user_obj)
+        newfile.save()
+
+        times_left -= 1
+        response['msg'] = '词单生成成功~今日剩余次数：' + str(times_left)
+        response['last_times'] = times_left
+
+        user_obj.gpt_lock = ""
+        user_obj.save()
+        return wrapRes(response, user_id)
 
 
+    except Exception as e:
+        response['msg'] = str(e)
+    try:
+
+        user_obj = UserInfo.objects.get(id=user_id)
+        user_obj.gpt_lock = ""
+        user_obj.save()
     except Exception as e:
         response['msg'] = str(e)
     return JsonResponse(response)
@@ -65,10 +101,13 @@ def get_wordList_smart_from_file(request):
     response = {}
     response['state'] = False
 
+    user_id_tmp = -1
+
     try:
         str_tmp = str(request.FILES.get('userId').read())[2:-1].replace("\"", "")
         print(str_tmp)
         user_id = int(str_tmp)
+        user_id_tmp=user_id
 
         if not checkCookie(request,response,user_id):
             return JsonResponse(response)
@@ -77,6 +116,19 @@ def get_wordList_smart_from_file(request):
 
         file = request.FILES.get('file')
         newfile = FileInfo(file_info=file,user_id=user_obj)
+
+        times_left = dailly_times - FileInfo.objects.filter(user_id_id=user_obj,
+                                                               post_time__gte=datetime.date.today()).count()
+
+        if user_obj.vip_time and user_obj.vip_time > datetime.datetime.now():
+            times_left += 2
+
+        if times_left == 0:
+            response['last_times'] = 0
+            response['msg'] = '今天的词单创建次数已经用完啦！明天再来吧'
+            user_obj.gpt_lock = ""
+            user_obj.save()
+            return JsonResponse(response)
 
         newfile.save()
 
@@ -125,9 +177,24 @@ def get_wordList_smart_from_file(request):
 
         response['wordlist']=ret
         response['state'] = True
-        response['msg'] = '词单提取成功'
 
 
+        times_left -= 1
+        response['msg'] = '词单提取成功~今日剩余次数：' + str(times_left)
+        response['last_times'] = times_left
+
+        user_obj.gpt_lock = ""
+        user_obj.save()
+        return wrapRes(response, user_id)
+
+
+    except Exception as e:
+        response['msg'] = str(e)
+    try:
+        if user_id_tmp!=-1:
+            user_obj = UserInfo.objects.get(id=user_id_tmp)
+            user_obj.gpt_lock = ""
+            user_obj.save()
     except Exception as e:
         response['msg'] = str(e)
     return JsonResponse(response)
